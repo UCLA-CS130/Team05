7098df10856de88fb2c8a603b8b24978c3ef4e29
Alfred Lucero <alfred.lucero@ucla.edu>
2017-02-05 12:29:56 -0800

    Created NginxServerConfigParser class to handle parsing of request handlers into a vector and server settings into a struct from a config file

--- server_config_parser.cc
+++ server_config_parser.cc
@@ -2,6 +2,54 @@
 #include "http_handler_echo.h"
 #include "http_handler_file.h"
 
+// Constructor: pass in a config file to be parsed for its contents
+NginxServerConfigParser::NginxServerConfigParser(const char* config_file) {
+    // Parses the config file into statements and tokens for us to extract data
+    config_parser.Parse(config_file, &config);
+}
+
+// Parses all server echo/static file request handlers and stores the parsed
+// handlers in a vector out-param. Returns number of request handlers found
+int NginxServerConfigParser::parseRequestHandlers(
+    std::vector<http::handler *>* handlers_out) {
+    /*
+    // Check config file for echo/static file requests to add
+    for (size_t i = 0; i < config.statements.size(); i++) {
+        // Add echo handler with its base URL
+        if (config.statements[i]->tokens[0] == "echo") {
+            http::handler_echo handler(config.statements[i]->tokens[1]);
+            handlers_out->push_back(&handler);
+        // Add static file with its root directory and base URL
+        } else if (config.statements[i]->tokens[0] == "static_serve") {
+            http::handler_file handler(config.statements[i]->tokens[2],
+                                       config.statements[i]->tokens[1]);
+            handlers_out->push_back(&handler);
+        }
+    }
+    
+    return handlers_out->size();
+    */
+    return 1;
+}
+
+// Parses all server settings and stores the parsed server setup in 
+// server_config out-parm. Returns port number (-1 if no number is found)
+int NginxServerConfigParser::parseServerSettings(
+    server_config* server_setings_out) {
+    /*
+    server_settings_out->port = -1;
+   
+    for (size_t i = 0; i < config.statements.size(); i++) {
+        if (config.statements[i]->tokens[0] == "port") {
+            server_settings_out->port = 
+                                  std::stoi(config.statements[i]->tokens[1]);
+        }
+    }
+ 
+    return server_settings_out->port;
+    */
+    return 1;
+}
 
 // Constructor: pass in a config file to be parsed for its contents
 NginxServerConfigParser::NginxServerConfigParser(const char* config_file) {
